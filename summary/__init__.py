650bd4c3b7feef9d19ed199e9a7653271386e13f
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2014-05-25 16:47:02 +0200

    Extract from <head>, then <body> if necessary

--- summary/__init__.py
+++ summary/__init__.py
@@ -9,6 +9,9 @@ tag first, applying specific head extraction techniques, and
 goes on to the <body> only if Summary data is not complete.
 """
 import extraction, requests
+from contextlib import closing
+
+CHUNK_SIZE = 1024
 
 class Summary(object):
 	"Provides incremental load mechanism and validation."
@@ -30,43 +33,44 @@ class Summary(object):
 		self.images = []
 		self.urls = []
 
-	@property
-	def title(self):
-		"Return the best title, if any."
-		if self.titles:
-			return self.titles[0]
-		else:
-			return None
+	# # Non-plural properties
+		# @property
+		# def title(self):
+		# 	"Return the best title, if any."
+		# 	if self.titles:
+		# 		return self.titles[0]
+		# 	else:
+		# 		return None
 
-	@property
-	def description(self):
-		"Return the best description, if any."
-		if self.descriptions:
-			return self.descriptions[0]
-		else:
-			return None
+		# @property
+		# def description(self):
+		# 	"Return the best description, if any."
+		# 	if self.descriptions:
+		# 		return self.descriptions[0]
+		# 	else:
+		# 		return None
 
-	@property
-	def image(self):
-		"Return the best image, if any."
-		if self.images:
-			return self.images[0]
-		else:
-			return None
+		# @property
+		# def image(self):
+		# 	"Return the best image, if any."
+		# 	if self.images:
+		# 		return self.images[0]
+		# 	else:
+		# 		return None
 
-	@property
-	def url(self):
-		"Return the best url, if any."
-		if self.urls:
-			return self.urls[0]
-		else:
-			return None
-
+		# @property
+		# def url(self):
+		# 	"Return the best url, if any."
+		# 	if self.urls:
+		# 		return self.urls[0]
+		# 	else:
+		# 		return None
 
 	def _is_complete(self):
 		# return False
-		return self.title and self.description and self.image and self.url \
-			and True
+		return self.titles and self.descriptions \
+			and self.images and self.urls and True
+			# self.title and self.description and self.image and self.url \
 
 	def _load(self, titles=[], descriptions=[], images=[], urls=[], **kwargs):
 		"""
@@ -82,21 +86,85 @@ class Summary(object):
 
 		# TODO: set non-plural fields to best item
 
+	def _clean_url(self, url):
+		"Fixes the url, but it should also discard useless query params."
+		import urllib
+		import urlparse
+		def url_fix(s, charset='utf-8'):
+		    "https://github.com/mitsuhiko/werkzeug/blob/master/werkzeug/urls.py"
+		    if isinstance(s, unicode):
+		        s = s.encode(charset, 'ignore')
+		    scheme, netloc, path, qs, anchor = urlparse.urlsplit(s)
+		    path = urllib.quote(path, '/%')
+		    qs = urllib.quote_plus(qs, ':&=')
+		    return urlparse.urlunsplit((scheme, netloc, path, qs, anchor))
+		return url_fix(url)
 
-	def extract(self):
+	def _get_tag(self, response, tag_name="html"):
+		"Iterates response content and returns the tag if found."
+		tag_start = tag_end = None
+		def find_tag(html, tag_name, tag_start, tag_end):
+			if not tag_start:
+				start = html.find("<%s" % tag_name)
+				if start >= 0: tag_start = start
+			if tag_start:
+				end = html.find("</%s>" % tag_name)
+				if end > tag_start: tag_end = end+len(tag_name)+3
+			if tag_end: # and tag_start
+				return html[tag_start:tag_end]
+			return None
+		for chunk in response.iter_content(CHUNK_SIZE, decode_unicode=True):
+			self._html += chunk
+			tag = find_tag(self._html.lower(), tag_name, tag_start, tag_end)
+			if tag:
+				return tag
+		tag = find_tag(self._html.lower(), tag_name, tag_start, tag_end)
+		return tag
+
+	def extract(self, check_url=None):
 		"""
-		Download the HTML <head> tag first, load it and see 
-		if Summary data is complete. Otherwise download the HTML <body> 
-		as well and load it by passing the appropriate techniques.
+		Downloads HTML <head> tag first, extracts data from it using
+		specific head techniques, loads it and checks if is complete. 
+		Otherwise downloads the HTML <body> tag as well and loads data 
+		extracted by using appropriate semantic techniques.
+
+		Eagerly calls check_url(url) if any, before parsing the HTML.
+		Provided function should throw an exception to break extraction.
+		E.g.: URL has been summarized before; URL points to off limits
+		websites like facebook.com and so on.
 		"""
-		extractor = extraction.Extractor(techniques=
-			["extraction.techniques.FacebookOpengraphTags",
-			 "extraction.techniques.HeadTags"])
+		url = self.source_url
+		with closing(requests.get(url, stream=True, timeout=10)) as response:
+			response.raise_for_status()
+			# TODO: validate content-type
+			# response.headers.get('content-type')
 
-		page = requests.get(self.source_url, timeout=10)
-		page.raise_for_status()
+			url = self._clean_url(response.url)
+			if check_url:
+				check_url(url)
 
-		extracted = extractor.extract(page.text, source_url=page.url)
+			self._html = u""
+			head = self._get_tag(response, tag_name="head")
+			# print "Get head: %s" % len(head)
+			extractor = extraction.Extractor(techniques=[
+				"extraction.techniques.FacebookOpengraphTags",
+		        "extraction.techniques.TwitterSummaryCardTags",
+				"extraction.techniques.HeadTags"
+			])
+			extracted = extractor.extract(head, source_url=url)
+			self._load(**extracted)
 
-		self._load(**extracted)
+			if self._is_complete():
+				return # done
+
+			body = self._get_tag(response, tag_name="body")
+			# print "Get body: %s" % len(body)
+			extractor = extraction.Extractor(techniques=[
+		        "extraction.techniques.HTML5SemanticTags",
+		        "extraction.techniques.SemanticTags"				
+			])
+			extracted = extractor.extract(body, source_url=url)
+			self._load(**extracted)
+
+		# that's it
 
