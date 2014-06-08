c53cbef533cfa0ac7b4720a68eb722ea72b07cc7
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2014-06-08 18:46:31 +0200

    PyPI preparations

---
+++ setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='summary-extraction',
+    version='0.1',
+    author='Alexandru Stanciu',
+    author_email='alexandru.stanciu@gmail.com',
+    packages=['summary'],
+    license='LICENSE.txt',
+    url='https://github.com/svven/summary',
+    # download_url = 'https://github.com/svven/summary/tarball/0.1',
+    description='Extract the title, image and description from any URL.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'Pillow >= 2.4.0',
+        'adblockparser >= 0.2',
+        'extraction >= 0.2',
+        'lxml >= 3.3.5',
+        # 're2 >= 0.2.20',
+        'requests >= 2.2.1',
+    ],
+)
