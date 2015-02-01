c78ae447a1ab9a2cc384abd1aa51b35b4baf8920
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-02-01 14:44:23 +0100

    All indentation to spaces

--- summarize.py
+++ summarize.py
@@ -4,78 +4,78 @@ and renders the output in HTML format as news articles.
 """
 
 def render(template, **kwargs):
-	"""
-	Renders the HTML containing provided summaries.
+    """
+    Renders the HTML containing provided summaries.
 
-	The summary has to be an instance of summary.Summary, 
-	or at least contain similar properties: title, image, url,
-	description and collections: titles, images, descriptions.
-	"""
-	import jinja2
-	import os.path as path
+    The summary has to be an instance of summary.Summary, 
+    or at least contain similar properties: title, image, url,
+    description and collections: titles, images, descriptions.
+    """
+    import jinja2
+    import os.path as path
 
-	searchpath = path.join(path.dirname(__file__), 
-		"templates") 
-	loader = jinja2.FileSystemLoader(searchpath=searchpath)
-	env = jinja2.Environment(loader=loader)
-	temp = env.get_template(template)
+    searchpath = path.join(path.dirname(__file__), 
+        "templates") 
+    loader = jinja2.FileSystemLoader(searchpath=searchpath)
+    env = jinja2.Environment(loader=loader)
+    temp = env.get_template(template)
 
-	return temp.render(**kwargs)
+    return temp.render(**kwargs)
 
 
 def summarize(urls):
-	"""
-	Calls extract for each of the URLs,
-	Returns the list of Extracted instances as summaries, 
-	the result of the process, and the speed.
-	"""
-	import time
-	from summary import Summary
+    """
+    Calls extract for each of the URLs,
+    Returns the list of Extracted instances as summaries, 
+    the result of the process, and the speed.
+    """
+    import time
+    from summary import Summary
 
 
-	fails = 0
-	err = lambda e: e.__class__.__name__
+    fails = 0
+    err = lambda e: e.__class__.__name__
 
-	summaries = []
-	start = time.time()
-	for url in urls:
-		try:
-			print "-> %s" % url
-			summary = Summary(url)
-			summary.extract()
-		except KeyboardInterrupt:
-			break
-		except Exception, e:
-			fails += 1
-			summary = {
-				'titles': ["[%s]" % err(e)],
-				'urls': [url],
-				'descriptions': [str(e)],
-				'source': url,
-				}
-			print "[%s] (%s): %s" % (err(e), e, url)
-		summaries.append(summary)
-		end = time.time()
+    summaries = []
+    start = time.time()
+    for url in urls:
+        try:
+            print "-> %s" % url
+            summary = Summary(url)
+            summary.extract()
+        except KeyboardInterrupt:
+            break
+        except Exception, e:
+            fails += 1
+            summary = {
+                'titles': ["[%s]" % err(e)],
+                'urls': [url],
+                'descriptions': [str(e)],
+                'source': url,
+                }
+            print "[%s] (%s): %s" % (err(e), e, url)
+        summaries.append(summary)
+        end = time.time()
 
-	result = fails and "Fails: %s out of %s." % (fails, len(summaries)) \
-		or "Success: %s." % len(summaries)
-	print result
+    result = fails and "Fails: %s out of %s." % (fails, len(summaries)) \
+        or "Success: %s." % len(summaries)
+    print result
 
-	duration = end - start
-	speed = "%.2f" % (duration/len(summaries))
+    duration = end - start
+    speed = "%.2f" % (duration/len(summaries))
 
-	return summaries, result, speed
+    return summaries, result, speed
 
 
 if __name__ == '__main__':
-	urls = []
-	with open('urls.txt', 'r') as file:
-		urls.extend([url.strip() for url in file])
+    urls = []
+    with open('urls.txt', 'r') as file:
+        urls.extend([url.strip() for url in file])
 
-	summaries, result, speed = summarize(urls)
-	page = render(template="news.html",
-		summaries=summaries, result=result, speed=speed)
+    summaries, result, speed = summarize(urls)
+    page = render(template="news.html",
+        summaries=summaries, result=result, speed=speed)
 
-	with open('news.html', 'w') as file:
-		file.write(page.encode('utf-8'))
+    with open('news.html', 'w') as file:
+        file.write(page.encode('utf-8'))
 
--- summary/request.py
+++ summary/request.py
@@ -9,17 +9,17 @@ from config import USER_AGENT, TIMEOUT
 requests.packages.urllib3.disable_warnings()
 
 def get(url, **kwargs):
-	"""
-	Wrapper for `request.get` function to set params.
-	"""
-	headers = kwargs.get('headers', {})
-	headers['User-Agent'] = USER_AGENT # overwrite
-	kwargs['headers'] = headers
+    """
+    Wrapper for `request.get` function to set params.
+    """
+    headers = kwargs.get('headers', {})
+    headers['User-Agent'] = USER_AGENT # overwrite
+    kwargs['headers'] = headers
 
-	timeout = kwargs.get('timeout', TIMEOUT)
-	kwargs['timeout'] = timeout
+    timeout = kwargs.get('timeout', TIMEOUT)
+    kwargs['timeout'] = timeout
 
-	kwargs['verify'] = False # no SSLError
+    kwargs['verify'] = False # no SSLError
 
-	logger.debug("Getting: %s", url)
-	return requests.get(url, **kwargs)
+    logger.debug("Getting: %s", url)
+    return requests.get(url, **kwargs)
