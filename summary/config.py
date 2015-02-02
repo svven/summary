15e72e9b954fa4e45421a020b8226c2100909a00
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-02-03 00:34:14 +0100

    Config from object for easy update

--- summary/config.py
+++ summary/config.py
@@ -2,6 +2,19 @@
 Summary config settings.
 These can be overwritten after importing `summary` and before using it.
 """
+def from_object(updates):
+    "Update same name (or prefixed) settings."
+    import sys
+    config = sys.modules[__name__]
+    
+    prefix = config.__name__.split('.')[0].upper()
+    keys = [k for k in config.__dict__ if \
+        k != from_object.__name__ and not k.startswith('_')]
+    get_value = lambda c, k: hasattr(c, k) and getattr(c, k) or None
+    for key in keys:
+        prefix_key = '%s_%s' % (prefix, key)
+        value = get_value(updates, prefix_key) or get_value(updates, key)
+        if value: setattr(config, key, value)
 
 ### Package settings ###
 
